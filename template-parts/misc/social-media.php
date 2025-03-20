8b64d97e4568b392681bc98732f683088ca77688
salvimateus <salvi.mateus@gmail.com>
2025-03-20 01:19:10 -0300

    fix(social icons): add dark mode color

--- template-parts/misc/social-media.php
+++ template-parts/misc/social-media.php
@@ -5,14 +5,14 @@
 
 <div class="s-social-main-link">
   <a href="https://instagram.com/salvimateus" target="_blank">
-    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/instagram-icon.svg" alt="Our Instagram" />
+    <img class="svg-icon" src="<?php echo get_template_directory_uri(); ?>/assets/images/instagram-icon.svg" alt="Our Instagram" />
   </a>
 
   <a href="mailto:salvi.mateus@gmail.com">
-    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/email-icon.svg" alt="Our email" />
+    <img class="svg-icon" src="<?php echo get_template_directory_uri(); ?>/assets/images/email-icon.svg" alt="Our email" />
   </a>
 
   <a href="https://avante.centive.com.br" target="_blank">
-    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/share-icon.svg" alt="Our blog" />
+    <img class="svg-icon" src="<?php echo get_template_directory_uri(); ?>/assets/images/share-icon.svg" alt="Our blog" />
   </a>
 </div>
